da70377a89ae1e882499e74d77f1a155d1b24b08
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-02-03 14:33:48 -0300

    Fix base model.

--- model/base_model.py
+++ model/base_model.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel(Base):
     __abstract__ = True
     id_ = Column('id', Integer, primary_key=True)
