43f6e9ccff2a5a4592330d3f21e6b5b41ddc1ba5
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-02-03 14:31:03 -0300

    Created dao, controller and tests of category.

---
+++ controller/base_controller.py
@@ -0,0 +1,26 @@
+from dao.base_dao import BaseDao
+from model.base_model import BaseModel
+
+
+
+class BaseController:
+    def __init__(self, dao: BaseDao) -> None:
+        self.__dao = dao
+                
+    def create(self, model: BaseModel) -> None:
+        self.__dao.save(model)
+       
+    def read_by_id(self, id: int) -> BaseModel:
+        result = self.__dao.read_by_id(id)
+        return result
+
+    def read_all(self) -> list:
+        result = self.__dao.read_all()
+        return result
+
+    def delete(self, id: int) -> None:
+        item = self.read_by_id(id)
+        self.__dao.delete(item)
+        
+    def update(self, model: BaseModel) -> None:
+        self.__dao.save(model)
